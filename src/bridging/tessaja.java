f15dd54391d69835780fe7de8a2d9d5615b3a944
Rayandra Yala Pratama <rayandra.pratama@gmail.com>
2022-05-12 08:18:36 +0700

    add src folder
    add src folder

---
+++ src/bridging/tessaja.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridging;
+
+/**
+ *
+ * @author khanzasoft
+ */
+public class tessaja {
+    public static ApiBRI api=new ApiBRI();
+    public static void main(String[] args) {
+         api.buatVA("2021011500001","Paijo","200000","tes");
+    }
+    
+}
